e55312a2a955c3b00c40b449a3d32907bb89b70c
Brandon Furr <brandon.furr123@gmail.com>
2019-10-21 23:53:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
-  # write code here
+  def initialize(side_1:, side_2:, side_3:)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind(type)
+    if @side_1 && @side_2 = @side_3 
+      :equilateral 
+      elsif @side_1 + @side
+    
 end
