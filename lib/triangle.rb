58a0a9b2f3367e74c5da28e91219925d5955fba2
Brandon Furr <brandon.furr123@gmail.com>
2019-10-22 00:09:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
     if @side_1 + @side_2 < @side_3 || @side_2 + @side_3 < @side_1 || @side_1 + @side_3 < @side_2 || if @side_1 || @side_2 || @side_3 == 0 
       begin 
       raise KindError
+      rescue KindError => error 
+      
     if @side_1 && @side_2 == @side_3 
       :equilateral 
       elsif @side_1 && @side_2 !== @side_3
