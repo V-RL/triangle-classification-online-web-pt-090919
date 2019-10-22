8ce6db66f887e1c3ddae9418894a5aeae117acb3
Brandon Furr <brandon.furr123@gmail.com>
2019-10-22 00:03:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,11 +6,14 @@ class Triangle
   end
   
   def kind
+    
     if @side_1 && @side_2 == @side_3 
       :equilateral 
       elsif @side_1 && @side_2 !== @side_3
       :isosceles 
     elsif  @side_1 !== @side_2 && @side_2 !== @side_3 && @side_1 !== @side_3
       :scalene
+    end
+  end
     
 end
